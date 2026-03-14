08aef85892525c1fd9a4719370b10ee030fb3cbb
Beauty Johnson <beautybabe344444@gmail.com>
2026-03-14 10:53:27 -0700

    voice room fix

--- server.js
+++ server.js
@@ -1,149 +1,28 @@
+const express = require("express");
+const http = require("http");
 const WebSocket = require("ws");
+const path = require("path");
 
-const server = new WebSocket.Server({port:3000});
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-let rooms = {};
+// serve website files
+app.use(express.static(path.join(__dirname)));
 
-server.on("connection",socket=>{
-
-socket.on("message",message=>{
-
-const data = JSON.parse(message);
-
-
-
-if(data.type === "join"){
-
-socket.room = data.room;
-socket.name = data.name;
-
-if(!rooms[socket.room]){
-
-rooms[socket.room] = {
-users:[],
-seats:Array(8).fill(null),
-host:socket.name
-};
-
-}
-
-rooms[socket.room].users.push(socket);
-
-updateRoom(socket.room);
-
-return;
-
-}
-
-
-
-if(data.type === "takeSeat"){
-
-const room = rooms[socket.room];
-
-if(!room) return;
-
-const seatIndex = data.seat;
-
-if(!room.seats[seatIndex]){
-
-room.seats[seatIndex] = socket.name;
-
-updateRoom(socket.room);
-
-}
-
-}
-
-
-
-if(data.type === "raiseHand"){
-
-const room = rooms[socket.room];
-
-room.users.forEach(client=>{
-
-client.send(JSON.stringify({
-type:"handRaised",
-name:socket.name
-}));
-
+// websocket voice signaling
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
 });
 
-}
+const PORT = process.env.PORT || 3000;
 
-
-
-if(data.type === "gift"){
-
-const room = rooms[socket.room];
-
-room.users.forEach(client=>{
-
-client.send(JSON.stringify({
-type:"gift",
-amount:data.amount
-}));
-
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
-
-}
-
-
-
-if(socket.room && rooms[socket.room]){
-
-rooms[socket.room].users.forEach(client=>{
-
-if(client !== socket && client.readyState === WebSocket.OPEN){
-
-client.send(JSON.stringify(data));
-
-}
-
-});
-
-}
-
-});
-
-
-socket.on("close",()=>{
-
-if(!socket.room) return;
-
-const room = rooms[socket.room];
-
-room.users = room.users.filter(c=>c !== socket);
-
-room.seats = room.seats.map(seat =>
-seat === socket.name ? null : seat
-);
-
-updateRoom(socket.room);
-
-});
-
-});
-
-
-function updateRoom(roomName){
-
-const room = rooms[roomName];
-
-const users = room.users.map(c=>c.name);
-
-room.users.forEach(client=>{
-
-client.send(JSON.stringify({
-type:"roomUpdate",
-users:users,
-seats:room.seats,
-host:room.host
-}));
-
-});
-
-}
-
-console.log("Signaling server running on port 3000");
